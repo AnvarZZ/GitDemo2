0dd1f16dfe828a4bb8b3fdb0d703be7832ddb34f
AnvarZZ <anvarzeynalzade@gmail.com>
2022-05-05 16:25:33 +0400

    Add new Activity

--- app/src/main/java/az/brains/gitdemo2/MainActivity.kt
+++ app/src/main/java/az/brains/gitdemo2/MainActivity.kt
@@ -1,11 +1,22 @@
 package az.brains.gitdemo2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv.setOnClickListener {
+            Intent(this, MainActivity2::class.java).apply { startActivity(this) }
+        }
+
+    }
+
+    override fun onClick(p0: View?) {
+
     }
 }
